051f893b84f0e321aac4361c689e2c2a8d5b9c72
gleamsoda <gleamsoda99@gmail.com>
2023-05-19 10:23:08 +0900

    Update install.sh

--- osx-install.sh
+++ osx-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if ! command -v git >/dev/null 2>&1; then
+	xcode-select --install
+fi
+
 if ! command -v brew >/dev/null 2>&1; then
 	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 fi
