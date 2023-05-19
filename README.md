d9301066660733ba82cb175841ddfa38ba666450
gleamsoda <gleamsoda99@gmail.com>
2023-05-19 11:34:11 +0900

    Add Brewfile

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dotfiles
+
+以下のアプリケーションは公式に brew 経由でのインストールが提示されていないため、手動でインストールしておく
+
+- google-chrome
+- google-japanese-ime
+- alacritty
+- visual-studio-code
+- tableplus
+- docker
+- slack
+- discord
+- spotify
--- osx-install.sh
+++ osx-install.sh
@@ -7,3 +7,10 @@ fi
 if ! command -v brew >/dev/null 2>&1; then
 	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 fi
+
+SOURCE_DIR=$(cd "$(dirname "$0")"; pwd -P)  || "${HOME}/dotfiles"
+if [ -f "${SOURCE_DIR}/Brewfile" ]; then
+	brew bundle --file="${SOURCE_DIR}/Brewfile"
+else
+	echo "Brewfile not found."
+fi
